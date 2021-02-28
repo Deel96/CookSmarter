bd1f0b34b010876834c1815141940150e8af207e
Dennis Eller <dennis.eller@mail.de>
2021-02-28 20:59:58 +0100

    asd

--- server.ts
+++ server.ts
@@ -1,68 +1,18 @@
+import { serve } from "https://deno.land/std@0.83.0/http/server.ts";
+import * as flags from "https://deno.land/std@0.83.0/flags/mod.ts";
 
-import { Database, MySQLConnector, Model, DataTypes } from 'https://deno.land/x/denodb/mod.ts';
+const DEFAULT_PORT = 8080;
+const argPort = flags.parse(Deno.args).port;
+const port = argPort ? Number(argPort) : DEFAULT_PORT;
 
-const connector = new MySQLConnector({
-  database: 'f28onj0nw73cwp7m',
-  host: 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  username: 'vy6seuao736kgme9',
-  password: 'zzxoo4wj2ptj9kwa',
-  port: 3306, // optional
-});
-
-const db = new Database(connector);
-
-class Flight extends Model {
-  static table = 'flights';
-  static timestamps = true;
-
-  static fields = {
-    id: { primaryKey: true, autoIncrement: true },
-    departure: DataTypes.STRING,
-    destination: DataTypes.STRING,
-    flightDuration: DataTypes.FLOAT,
-  };
-
-  static defaults = {
-    flightDuration: 2.5,
-  };
+if (isNaN(port)) {
+  console.error("Port is not a number.");
+  Deno.exit(1);
 }
 
-db.link([Flight]);
+const s = serve({ port: port });
+console.log("http://localhost:" + port);
 
-await db.sync({ drop: false });
-
-// await Flight.create({
-//   id:100,
-//   departure: 'Paris',
-//   destination: 'Tokyo',
-// });
-
-// or
-
-const flight = new Flight();
-flight.departure = 'London';
-flight.destination = 'San Francisco';
-await flight.save();
-
-await Flight.select('destination').all();
-// [ { destination: "Tokyo" }, { destination: "San Francisco" } ]
-
-await Flight.where('destination', 'Tokyo').delete();
-await Flight.where('destination', 'San Francisco').delete();
-
-
-const sfFlight = await Flight.select('destination').find(2);
-// { destination: "San Francisco" }
-
-await Flight.count();
-// 1
-
-let test = await Flight.select('id', 'destination').orderBy('id').get();
-console.log(test);
-// [ { id: "2", destination: "San Francisco" } ]
-
-//await sfFlight.delete();
-
-await db.close();
-
-
+for await (const req of s) {
+  req.respond({ body: "Hallo du\n" });
+}
